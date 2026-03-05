1779734fdacd25560760c78d8cfc3956aec3381d
rossirpaulo <rossir.paulo@gmail.com>
2026-03-05 01:05:29 -0600

    pretty logic to output nicely

---
+++ src/HaskProto/Pretty.hs
@@ -0,0 +1,53 @@
+module HaskProto.Pretty (prettySchema, prettyMessage, prettyField) where
+
+import HaskProto.Schema
+
+-- prettySchema
+-- Pretty-print an entire schema.
+prettySchema :: Schema -> String
+prettySchema (Schema msgs) =
+  unlines (concatMap (\m -> prettyMessage m ++ [""]) msgs)
+
+-- prettyMessage
+-- Pretty-print a single message definition.
+prettyMessage :: MessageDef -> [String]
+prettyMessage msg =
+  ["message " ++ msgName msg ++ " {"]
+    ++ map (\f -> "  " ++ prettyField f) (msgFields msg)
+    ++ ["}"]
+
+-- prettyField
+-- Pretty-print a single field definition.
+prettyField :: FieldDef -> String
+prettyField field =
+  fieldName field
+    ++ ": "
+    ++ prettyModifier (fieldModifier field)
+    ++ prettyType (fieldType field)
+    ++ " @"
+    ++ show (fieldTag field)
+
+-- prettyModifier
+-- Pretty-print a modifier (empty string for Required).
+prettyModifier :: Modifier -> String
+prettyModifier Required = ""
+prettyModifier Optional = "optional "
+prettyModifier Repeated = "repeated "
+
+-- prettyType
+-- Pretty-print a field type.
+prettyType :: FieldType -> String
+prettyType (PrimType prim) = prettyPrim prim
+prettyType (RefType name) = name
+
+-- prettyPrim
+-- Pretty-print a primitive type.
+prettyPrim :: PrimTy -> String
+prettyPrim TInt32 = "int32"
+prettyPrim TInt64 = "int64"
+prettyPrim TUInt32 = "uint32"
+prettyPrim TUInt64 = "uint64"
+prettyPrim TBool = "bool"
+prettyPrim TString = "string"
+prettyPrim TBytes = "bytes"
+prettyPrim TDouble = "double"
